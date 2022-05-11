4889c019c42e0187ba0cf1211f4f5cda4b115848
Cole Gibbs <colegibbs0@gmail.com>
2022-05-11 14:31:47 -0700

    written tests and all passing

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -2,14 +2,28 @@
 
 def encrypt(text, shift):
   shift = shift % 27
-  words = text.split()
+  # words = text.split()
   encrypted_text = ""
-  for word in words:
-    for letter in word:
-      if word.isalpha():
-        char_num = ord(letter)
-        encrypted_char = chr(char_num + shift)
-        encrypted_text += encrypted_char
+  for letter in text:
+    if letter.isalpha():
+      char_num = ord(letter)
+      if letter.isupper() and (char_num + shift) > 90:
+        char_num = ((char_num + shift) - 91) + 65
+        encrypted_char = chr(char_num)
+      elif letter.islower() and (char_num + shift) > 122:
+        char_num = ((char_num + shift) - 123) + 97
+        encrypted_char = chr(char_num)
       else:
-        encrypted_text += letter
+        encrypted_char = chr(char_num + shift)
+      encrypted_text += encrypted_char
+    else:
+      encrypted_text += letter
+  # for word in words:
+  #   for letter in word:
+  #     if letter.isalpha():
+  #       char_num = ord(letter)
+  #       encrypted_char = chr(char_num + shift)
+  #       encrypted_text += encrypted_char
+  #     else:
+  #       encrypted_text += letter
   return encrypted_text
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -7,4 +7,21 @@ def test_encrypt_all_alpha():
   assert actual == expected
 
 def test_encrypt_with_nonalpha():
-  actual = encrypt()
+  actual = encrypt('message$', 5)
+  expected = 'rjxxflj$'
+  assert actual == expected
+
+def test_encrypt_with_capital():
+  actual = encrypt('Message', 5)
+  expected = 'Rjxxflj'
+  assert actual == expected
+
+def test_encrypt_with_large_shift():
+  actual = encrypt('message', 29)
+  expected = 'oguucig'
+  assert actual == expected
+
+def test_encrypt_with_sentence():
+  actual = encrypt('This is my message.', 5)
+  expected = 'Ymnx nx rd rjxxflj.'
+  assert actual == expected
